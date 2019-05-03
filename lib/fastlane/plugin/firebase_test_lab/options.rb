ea30b426f6045e7293cce053bc76071b9ed7013e
lukedurrant <contact@lukedurrant.com>
2019-05-03 15:43:54 +1000

    ios_xc_test_args

--- lib/fastlane/plugin/firebase_test_lab/options.rb
+++ lib/fastlane/plugin/firebase_test_lab/options.rb
@@ -76,9 +76,9 @@ module Fastlane
                                          v = File.expand_path(value.to_s)
                                          UI.user_error!("Key file not found at path '#{v}'") unless File.exist?(v)
                                        end),
-          FastlaneCore::ConfigItem.new(key: :xcode_version,
-                                       description: "Xcode Version",
-                                       default_value: nil,
+          FastlaneCore::ConfigItem.new(key: :ios_xc_test_args,
+                                       description: "extra ios_xc_test_args useful for xcode version ie { xcodeVersion: '10.1' }",
+                                       default_value: {},
                                        optional: true),
         ]
       end
